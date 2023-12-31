fd6be9de35f3aac52768ee383b3246026897338c
Squarebug <developer@squarebug.pt>
2023-12-31 00:00:57 +0000

    1

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -1,20 +1,54 @@
-import Post from "/admin/preview-templates/post.js";
-import Page from "/admin/preview-templates/page.js";
+const PostPreviewCard = ({ entry, widgetFor }) => {
+  return h(
+    'div',
+    { style: { width: '100%' } },
+    widgetFor('image'),
+    h(
+      'div',
+      { style: { padding: '16px', width: '100%' } },
+      h(
+        'div',
+        {
+          style: {
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'space-between',
+            alignItems: 'start',
+          },
+        },
+        h(
+          'div',
+          {
+            style: {
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'baseline',
+              gap: '8px',
+            },
+          },
+          h('strong', { style: { fontSize: '24px' } }, entry.data.title),
+          h('span', { style: { fontSize: '16px' } }, entry.data.date),
+        ),
+        h(
+          'div',
+          {
+            style: {
+              backgroundColor: entry.data.draft === true ? 'blue' : 'green',
+              color: 'white',
+              border: 'none',
+              padding: '4px 8px',
+              textAlign: 'center',
+              textDecoration: 'none',
+              display: 'inline-block',
+              cursor: 'pointer',
+              borderRadius: '4px',
+            },
+          },
+          entry.data.draft === true ? 'Draft' : 'Published',
+        ),
+      ),
+    ),
+  );
+};
 
-// Register the Post component as the preview for entries in the blog collection
-CMS.registerPreviewTemplate("blog", Post);
-CMS.registerPreviewTemplate("pages", Page);
-
-CMS.registerPreviewStyle("/assets/main.css");
-// Register any CSS file on the home page as a preview style
-fetch("/")
-  .then(response => response.text())
-  .then(html => {
-    const f = document.createElement("html");
-    f.innerHTML = html;
-    Array.from(f.getElementsByTagName("link")).forEach(tag => {
-      if (tag.rel == "stylesheet" && !tag.media) {
-        CMS.registerPreviewStyle(tag.href);
-      }
-    });
-  });
+CMS.registerPreviewCard('posts', PostPreviewCard, () => 240);
